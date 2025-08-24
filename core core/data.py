94edab156914c9ab79afb7fe17c1ed30f826a062
Niklas2004-max <niklasschurmann244@gmail.com>
2025-08-24 13:31:09 +0200

    Delete core core directory

--- core core/data.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-def load_data(ticker: str):
-    """
-    Lädt 1 Jahr historische Daten von Yahoo Finance
-    """
-    return yf.download(ticker, period="1y")
