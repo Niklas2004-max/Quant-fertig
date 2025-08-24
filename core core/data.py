186aa1c1170eb1c39c7c6fea08ef1bf0ad3e6e4d
Niklas2004-max <niklasschurmann244@gmail.com>
2025-08-24 13:29:08 +0200

    Update data.py

--- core core/data.py
+++ core core/data.py
@@ -1,5 +1,8 @@
 import yfinance as yf
 import pandas as pd
 
-def load_data(ticker: str) -> pd.DataFrame:
+def load_data(ticker: str):
+    """
+    Lädt 1 Jahr historische Daten von Yahoo Finance
+    """
     return yf.download(ticker, period="1y")
